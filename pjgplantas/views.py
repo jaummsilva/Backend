cc28ee0ea43b37162549bc1b2f6269f668b09d68
= <=>
2022-09-22 15:56:11 -0300

    Cadastro e identificação de usuario

--- pjgplantas/views.py
+++ pjgplantas/views.py
@@ -24,6 +24,20 @@ from pjgplantas.serializers import (
     RegistrationSerializer,
 )
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['username'] = self.user.username
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 
 class RegistrationViewSet(ModelViewSet):
     queryset = User.objects.all()
